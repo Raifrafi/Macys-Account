89fe155408045932d5e91e22d830fa7a480a404a
Raifrafi <shalaha.rita03@gmail.com>
2023-12-04 21:21:44 -0500

    macys online

---
+++ src/test/java/com/macys/login/OpenBrowserMacys.java
@@ -0,0 +1,28 @@
+package com.macys.login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenBrowserMacys {
+
+	public static void main(String[] args) {
+		
+  WebDriverManager.chromedriver().setup();
+  WebDriver driver =new ChromeDriver();
+  driver.get("https://macys.com/");
+  
+  System.out.println(driver.getTitle());
+  System.out.println(driver.getCurrentUrl());
+  
+  
+  driver.close();
+		
+		
+		
+		
+
+	}
+
+}
